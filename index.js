a653dc7877d5438adfc866ad490b22d31623ce01
nanggo <yamsiri@gmail.com>
2020-12-22 01:15:49 +0900

    Implement login feature

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5000;
-const mongoose = require("mongoose");
-const { User } = require("./models/User");
-const bodyParser = require("body-parser");
-const config = require("./config/key");
+const mongoose = require('mongoose');
+const {User} = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const cookieParser = require('cookie-parser');
+
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // application/json
 app.use(bodyParser.json());
@@ -18,20 +20,40 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post("/register", (request, response) => {
+app.post('/register', (request, response) => {
   const user = new User(request.body);
 
   user.save((err, userInfo) => {
-    if (err) return response.json({ success: false, err });
-    return response.status(200).json({ success: true });
+    if (err) return response.json({success: false, err});
+    return response.status(200).json({success: true});
   });
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+app.post('/login', function (req, response) {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (err) return response.json({loginSuccess: false, message: err.message});
+    if (!user) {
+      response.json({
+        loginSuccess: false,
+        message: '해당하는 이메일이 존재하지 않습니다.',
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (err) return response.status(400).json({loginSuccess: false, message: err.message});
+      if (!isMatch) return response.json({loginSuccess: false, message: '비밀번호가 일치하지 않습니다.'});
+
+      // 비번 매칭됐을 때
+      user.generateToken((err, result) => {
+        if (err) return response.status(400).send(err);
+        response.status(200).json({loginSuccess: true, message: result._id});
+      });
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
+const secretKey = 'secretKey';
 
 const userSchema = mongoose.Schema({
   name: {
@@ -33,22 +35,43 @@ const userSchema = mongoose.Schema({
   },
 });
 
-
-
-userSchema.pre('save',function(next){
+userSchema.pre('save', function (next) {
   var user = this;
-  
-  if(user.isModified('password')){
+
+  if (user.isModified('password')) {
     bcrypt.genSalt(saltRounds, (err, salt) => {
-      if(err) return next(err);
+      if (err) return next(err);
       bcrypt.hash(user.password, salt, (err, encrypted) => {
-        if(err) return next(err);
-        user.password = salt;
+        if (err) return next(err);
+        user.password = encrypted;
         next();
-      })
-    })
+      });
+    });
+  } else {
+    next();
   }
 });
-const User = mongoose.model("User", userSchema);
 
-module.exports = { User };
+userSchema.methods.comparePassword = function (plainText, cb) {
+  console.log('plainText: ', plainText);
+  console.log('encrypted: ', this.password);
+  bcrypt
+    .compare(plainText, this.password)
+    .then(result => cb(null, result))
+    .catch(error => cb(error));
+};
+
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), secretKey);
+  user.token = token;
+
+  user.save((err, res) => {
+    if (err) return cb(err);
+    cb(null, res);
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
