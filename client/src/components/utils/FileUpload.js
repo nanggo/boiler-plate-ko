d9e7853d8105172bb08d020190d7e39719e70ce5
nanggo <yamsiri@gmail.com>
2021-01-05 02:40:43 +0900

    Add file drop handler

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import {PlusOutlined} from '@ant-design/icons';
+import axios from 'axios';
 
 function FileUpload() {
+  const fileDropHandler = files => {
+    const formData = new FormData();
+    formData.append('file', files[0]);
+    const config = {
+      header: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios
+      .post('/api/product/image', formData, config)
+      .then(res => {
+        if (res.data.success) {
+        } else {
+          alert('파일 업로드 실패');
+        }
+      })
+      .catch(err => alert(err.message));
+  };
   return (
     <div style={{display: 'flex', justifyContent: 'space-between'}}>
-      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      <Dropzone onDrop={fileDropHandler}>
         {({getRootProps, getInputProps}) => (
           <section>
             <div
